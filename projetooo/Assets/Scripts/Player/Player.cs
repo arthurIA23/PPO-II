13052ec11411687e3f96d26cf2a1e3ff872cdf92
arthurIA23 <arthurcaue023@gmail.com>
2025-07-18 19:54:20 -0300

    ..
    de fude esse commit

--- projetooo/Assets/Scripts/Player/Player.cs
+++ projetooo/Assets/Scripts/Player/Player.cs
@@ -9,7 +9,34 @@ public class Player : MonoBehaviour
     public float jumpForce = 6f;
     private float nJump;
     private BoxCollider2D bc2d;
+    // private Animator anime;
+    // private int runHash = Animator.StringToHash("mpRunning");
+    // private int jumpHash = Animator.StringToHash("mpJumping");
     [SerializeField] private LayerMask groundLayer;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bc2d = GetComponent<BoxCollider2D>();
+        // anime = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        
+        // anime.SetBool(runHash, rb.linearVelocity.x != 0);
+        // anime.SetBool(jumpHash, !isGrounded());
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+
     void Move()
     {
         if (Input.GetKey(KeyCode.D))
@@ -42,29 +69,12 @@ public class Player : MonoBehaviour
         }
     }
 
+
+
     private bool isGrounded()
     {
         RaycastHit2D ground = Physics2D.BoxCast(bc2d.bounds.center, bc2d.bounds.size, 0f, Vector2.down, 0.01f, groundLayer);
         return ground.collider != null;
-    }
-
-
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        bc2d = GetComponent<BoxCollider2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Jump();
-    }
-
-    void FixedUpdate()
-    {
-        Move();
     }
 
 
